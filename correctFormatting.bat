12d8f8934e5efa139b86a28b978d266249f87d69
Connor <25871918+ThermalAxis@users.noreply.github.com>
2024-02-12 15:56:49 +0000

    Add files via upload

---
+++ correctFormatting.bat
@@ -0,0 +1,29 @@
+@echo off
+setlocal enableextensions enabledelayedexpansion
+
+title Correct C Language Formatting
+echo Correct C Language Formatting
+echo.
+
+set /a i=0
+set /a j=0
+
+for %%a in (%*) do (
+set /a j+=1
+)
+
+for %%b in (%*) do  (
+set /a i+=1
+cls
+echo Correct C Language Formatting
+echo.
+echo Processing file !i! of %j%
+echo %%b
+echo.
+start /wait /B .\clang-format.exe -style=file .\msvc.conf -i %%b
+pause
+)
+
+cls
+echo Processed !i! of %j% files, press any key to exit.
+pause>nul
